fc52cf2da3f0609ee031c138407c9a1f2e6158ea
Leenzery <ly@leenzery.dev>
2023-12-17 18:48:54 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ var arrLang = {
     "Links": "Ссылки",
     "Donate": "Пожертвовать мне",
     "???": "Возможно, здесь будет что-то ещё",
-    "desc": getAge("2006/10/12") + " y.o. \n C#, Java разработчик \n пользуюсь Lumia 950 с Windows 10 Mobile \n я говорю/понимаю"
+    "desc": getAge("2006/10/12") + " y.o. \n C#, Java разработчик \n пользуюсь Lumia 950XL с Windows 10 Mobile \n я говорю/понимаю"
   },
   "uk": {
     "Links": "Посилання",
     "Donate": "Пожертвуй мені",
     "???": "Можливо, тут буде щось інше",
-    "desc": getAge("2006/10/12") + " y.o. \n C#, Java розробник \n користуюся Lumia 950 з Windows 10 Mobile \n Я говорю / розумію"
+    "desc": getAge("2006/10/12") + " y.o. \n C#, Java розробник \n користуюся LumiaXL 950 з Windows 10 Mobile \n Я говорю / розумію"
   },
   "en": {
     "Links": "Links",
     "Donate": "Donate to me",
     "???": "Perhaps there will be something else here",
-    "desc": getAge("2006/10/12") + " y.o. \n C#, Java Developer \n I use Lumia 950 with Windows 10 Mobile \n I speak / understand"
+    "desc": getAge("2006/10/12") + " y.o. \n C#, Java Developer \n I use LumiaXL 950 with Windows 10 Mobile \n I speak / understand"
   }
 };
 function getAge(dateString) {
